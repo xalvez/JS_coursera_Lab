6b2562840a8a12ce0867df62f62ddf31d90e4bc6
xalvez <xxalvezz@gmail.com>
2025-05-11 11:31:28 +0300

    count

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -6,5 +6,6 @@ function calculateArea() {
  width = parseFloat(document.getElementById('width').value);
 
  let area = length * width;
-}
+
  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+}
